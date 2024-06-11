a048abe9f19ca400c8ccd0fbdf54b4aa29fc3d68
子非鱼 <1950053842@qq.com>
2024-06-11 16:21:51 +0800

    解决 错误: 缺少 JavaFX 运行时组件, 需要使用该组件来运行此应用程序  问题

---
+++ src/main/java/meowdb/JavaFXBootstrap.java
@@ -0,0 +1,10 @@
+package meowdb;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import meowdb.boot.Bootstrap;
+public class JavaFXBootstrap {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
